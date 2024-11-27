82619070f00290fe590f2789db9a2d0aaa863157
net <luca.126>
2024-11-27 10:38:25 +0100

    aggiunta funzione di aggiunta e rimozione elemento

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+lista_spesa = []
 
 #rotta principale
 @app.route('/')
 def home():
-    return "Per ora funziona tutto"
+    return render_template('index.html', lista_spesa = lista_spesa)
+
+@app.route('/aggiungi', methods=['POST'])
+def aggiungi():
+    elemento = request.form['elemento']
+    if elemento:
+        lista_spesa.append(elemento)
+    return redirect(url_for('home'))
+
+@app.route('/rimuovi/<int:indice>', methods=['POST'])
+def rimuovi(indice):
+    if 0 <= indice < len(lista_spesa):
+        lista_spesa.pop(indice)
+    return redirect(url_for('home'))
 
 #avvio dell'app Flask
 if __name__ == '__main__':
